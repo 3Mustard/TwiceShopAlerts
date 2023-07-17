029ec3914938729153fe4797e05794dd8276f42a
Matthew Cullen <m23.cullen@gmail.com>
2023-07-16 22:21:36 -0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,133 @@
+// Dependencies
+const axios = require('axios');
+const mailer = require('nodemailer');
+const twilio = require('twilio');
+const dotenv = require('dotenv');
+
+// DEBUG MODE
+// SET DEBUG = TRUE
+var debug = false;
+
+// OPTIONS
+var sendSmsMsg = true;
+
+// Twice Shop URL
+var twiceShopUrl = "https://twiceshop.com/";
+var twiceShopJihyoCollectionUrl = 'https://twiceshop.com/collections/jihyo';
+var twiceShopNayeonCollectionUrl = 'https://twiceshop.com/collections/nayeon';
+// test website here 
+var debugUrl = "";
+
+// SEARCHES/KEYWORDS
+//
+var keywords = ['KILLIN', 'killin', 'Me Good', 'ME GOOD', 'Good', 'GOOD', 'KILLIN ME GOOD' , 'Killin Me Good', 'killin me good', 'KILLIN` ME GOOD', 'KILLIN\' ME GOOD'];
+// 'Killin' found in a comment ~ Killing any Ajax request that's...
+var debugKeyword = 'NAYEON';
+
+// time between checks 
+var queueTimeInterval = 600000; // 10 min?
+
+// TWILIO TEXTING SERVICES TRIAL ENDS IN 30 DAYS
+// 
+// START ~ 7/16/2023
+var twilioTrialStartDate = new Date(2023,7,16);
+var twilioTrialEndDate = twilioTrialStartDate.setDate(twilioTrialStartDate.getDate() + 30);
+console.log('Twilio texting services trial ends 30 days after ~ 2023,7,16' );
+console.log('To continue Twilio texting services please sign up or create a new trial account.')
+
+var TWILIO_SID = 'ACdcdd0123e91af32c0ba1ce31f7b3601e';
+var TWILIO_AUTH_TOKEN = 'fa36b745ecc36dc37aa5d53b2f4aedd6';
+var TWILIO_PHONE_NUMBER = '+18442606797';
+var MY_PHONE_NUMBER = '+15854147239';
+
+
+// START
+//
+if (debug){
+  console.log('Running debug functions..');
+
+  debugCheckKeyword();
+
+  checkCollection();
+
+} else {
+
+  setInterval(main, queueTimeInterval);
+  setInterval(checkCollection, queueTimeInterval);
+  console.log('Starting app');
+
+}
+
+function main(){
+  searchUrlForKeywords(twiceShopUrl, keywords);
+}
+
+function searchUrlForKeywords(URL, keywords){
+  console.log('searching..')
+
+  keywords.forEach((keyword)=>{
+    console.log('Checking ' + URL + ' for the keyword ' + keyword)
+
+    try {
+      axios.get(URL).then((response) =>{
+        if (response.data.includes(keyword)){
+          if (sendSmsMsg){
+            sendSms('KEYWORD: ' + keyword + ': FOUND WHEN SEARCHING ' + URL, TWILIO_PHONE_NUMBER, MY_PHONE_NUMBER);
+          }
+          else {
+            console.log('KEYWORD: ' + keyword + ': FOUND WHEN SEARCHING ' + URL);
+          }
+        }
+        else {
+          console.log('KEYWORD: ' + keyword + ': NOT found');
+        }
+      })
+    }
+    catch (ex) {
+      console.log(ex);
+    }
+  })
+}
+
+async function checkCollection(){
+  var collectionURL = debug ? twiceShopNayeonCollectionUrl : twiceShopJihyoCollectionUrl;
+  console.log('Checking collection: ' + collectionURL);
+
+  await axios.get(collectionURL).then((res)=>{
+    console.log('found collection')
+    if (sendSmsMsg){
+      sendSms('collection found', TWILIO_PHONE_NUMBER, MY_PHONE_NUMBER);
+    }
+    else {
+      console.log('Collection found at ' + collectionURL);
+    }
+  }).catch((err)=>{
+    console.log('Collection NOT found: 404');
+  })
+
+}
+
+function sendSms(message, fromNumber, toNumber){
+  console.log('Sending sms to ' + toNumber + '..');
+  const client = new twilio(TWILIO_SID, TWILIO_AUTH_TOKEN);
+  return client.messages.create({body: message, from: fromNumber, to: toNumber});
+}
+
+// DEBUG FUNCTIONS
+//
+function debugCheckKeyword(){
+  console.log('Checking ' + twiceShopUrl + ' for the keyword ' + debugKeyword);
+
+  axios.get(twiceShopUrl).then((response) =>{
+    if (response.data.includes(debugKeyword)){
+      if (sendSmsMsg){
+        sendSms('keyword found', TWILIO_PHONE_NUMBER, MY_PHONE_NUMBER);
+      }
+      else {
+        console.log('keyword found');
+      }
+    } else {
+      console.log('keyword not found');
+    }
+  })
+}
